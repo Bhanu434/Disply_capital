1b2871195e30030e751bdfdbfacece277cf7683f
Bhanu <sandrabhanuprakash@gmail.com>
2023-12-16 16:18:53 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -9,3 +9,19 @@ def display_capital(request):
     QLCO=Capital.objects.all()
     d={'QLCO':QLCO}
     return render(request,'display_capital.html',d)
+def insert_country(request):
+    cn=input('Enter country Name :- ')
+    CO=Country.objects.get_or_create(country_name=cn)[0]
+    CO.save()
+    QLCO=Country.objects.all()
+    d={'QLCO':QLCO}
+    return render(request,'display_country.html',d)
+def insert_capital(request):
+    cn=input('Enter country Name :- ')
+    c=input('Enter Capital Name :- ')
+    CO=Country.objects.get(country_name=cn)
+    C=Capital.objects.get_or_create(country_name=CO,city_name=c)[0]
+    C.save()
+    QLCO=Capital.objects.all()
+    d={'QLCO':QLCO}
+    return render(request,'display_capital.html',d)
